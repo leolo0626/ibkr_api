8cbcd4b996b15e3a5545b22168b8ed883b40819b
leolo <leolo0626@gmail.com>
2023-05-27 22:57:24 +0800

    add some thought

--- ibkr_connect/playground/bar_data.py
+++ ibkr_connect/playground/bar_data.py
@@ -15,19 +15,24 @@ bars = ib.reqHistoricalData(contract,
                             endDateTime='',
                             durationStr='600 S',
                             barSizeSetting='1 min',
-                            whatToShow='ADJUSTED_LAST',
+                            whatToShow='TRADES',
                             useRTH=False,
                             formatDate=1,
                             keepUpToDate=True)
 
 
 def onBarUpdate(bar, hasNewBar):
+    # When the bar update, we can place the signal function here
+    # Save the Price data for future update for future checking
+    # Ordering is also place here
+    # If order is placed, then we may need a messaging channel to notify the user in the group
     if hasNewBar:
         print(bar)
 
 
 bars.updateEvent += onBarUpdate
 
+# What if we don't cancel it, will it keep update the bar ??
 ib.sleep(60)
 ib.cancelHistoricalData(bars)
 
